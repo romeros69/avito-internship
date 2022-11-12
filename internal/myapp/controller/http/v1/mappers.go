a65cf0fb34ac5cb875cd4aa552c4b66b2184f26b
romeros69 <jefops@gmail.com>
2022-11-12 19:49:13 +0300

    feat: add mapper for get balance

---
+++ internal/myapp/controller/http/v1/mappers.go
@@ -0,0 +1,10 @@
+package v1
+
+import "strconv"
+
+func balanceToDTO(value int64) balanceResponseDTO {
+	return balanceResponseDTO{
+		Rubles:  strconv.FormatInt(value/100, 10),
+		Pennies: strconv.FormatInt(value%100, 10),
+	}
+}
